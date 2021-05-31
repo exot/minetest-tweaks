7301394023de3ddc91c130061e50e7fc2c0d8d87
Daniel Borchmann <daniel@algebra20.de>
2021-05-31 16:50:49 +0200

    Unify indentation in init.lua
    This is for consistency, the indentation has been done by Emacs.

--- init.lua
+++ init.lua
@@ -4,49 +4,49 @@ minetest.log("action", "[Minetest Tweaks] Loading …")
 -- Custom crafting recipes
 
 minetest.register_craft({
-  output = 'default:gravel 1',
-  recipe = {{'default:cobble'}},
+      output = 'default:gravel 1',
+      recipe = {{'default:cobble'}},
 })
 
 minetest.register_craft({
-  output = 'default:sand 1',
-  recipe = {{'default:gravel'}},
+      output = 'default:sand 1',
+      recipe = {{'default:gravel'}},
 })
 
 minetest.register_craft({
-  output = 'default:dirt 1',
-  recipe = {{'default:sand'}},
+      output = 'default:dirt 1',
+      recipe = {{'default:sand'}},
 })
 
 minetest.register_craft({
-  output = 'default:desert_sand 1',
-  recipe = {{'default:dirt'}},
+      output = 'default:desert_sand 1',
+      recipe = {{'default:dirt'}},
 })
 
 minetest.register_craft({
-  output = 'default:sand 1',
-  recipe = {{'default:desert_sand'}},
+      output = 'default:sand 1',
+      recipe = {{'default:desert_sand'}},
 })
 
 minetest.register_craft({
-  type = "shapeless",
-  output = "default:clay 4",
-  recipe = {"default:dirt", "default:dirt", "default:sand", "default:sand", "bucket:bucket_water"},
-  replacements = {
-     {"bucket:bucket_water", "bucket:bucket_empty"}
-  }
+      type = "shapeless",
+      output = "default:clay 4",
+      recipe = {"default:dirt", "default:dirt", "default:sand", "default:sand", "bucket:bucket_water"},
+      replacements = {
+         {"bucket:bucket_water", "bucket:bucket_empty"}
+      }
 })
 
 minetest.register_craft({
-  type = "shapeless",
-  output = "darkage:chalk 1",
-  recipe = {"darkage:chalk_powder", "darkage:chalk_powder"}
+      type = "shapeless",
+      output = "darkage:chalk 1",
+      recipe = {"darkage:chalk_powder", "darkage:chalk_powder"}
 })
 
 minetest.register_craft({
-  type = "shapeless",
-  output = "homedecor:oil_extract",
-  recipe = {"flowers:sunflower", "flowers:sunflower"}
+      type = "shapeless",
+      output = "homedecor:oil_extract",
+      recipe = {"flowers:sunflower", "flowers:sunflower"}
 })
 
 colors_from_plants = {
@@ -58,18 +58,18 @@ colors_from_plants = {
 
 for color,ingredients in pairs(colors_from_plants) do
    minetest.register_craft({
-      type = "shapeless",
-      output = color,
-      recipe = ingredients
+         type = "shapeless",
+         output = color,
+         recipe = ingredients
    })
 end
 
 minetest.register_craft({
-  output = "mobs:leather",
-  recipe = {{'basic_materials:plastic_sheet',
-             'basic_materials:plastic_sheet',
-             'basic_materials:plastic_sheet'},
-             {'group:wool', 'group:wool', 'group:wool'}}
+      output = "mobs:leather",
+      recipe = {{'basic_materials:plastic_sheet',
+                 'basic_materials:plastic_sheet',
+                 'basic_materials:plastic_sheet'},
+         {'group:wool', 'group:wool', 'group:wool'}}
 })
 
 -- When playing with petz, we get a new kind of leather.  Let's make it
